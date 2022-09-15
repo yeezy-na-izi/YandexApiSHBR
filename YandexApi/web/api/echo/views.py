6308794e223498594f7c563f44b86761eff92752
yeezy-na-izi <howone1sky@gmail.com>
2022-09-15 22:36:52 +0300

    Maybe так?

--- YandexApi/web/api/echo/views.py
+++ YandexApi/web/api/echo/views.py
@@ -5,14 +5,12 @@ from YandexApi.web.api.echo.schema import Message
 router = APIRouter()
 
 
-@router.post("/", response_model=Message)
-async def send_echo_message(
-    incoming_message: Message,
-) -> Message:
+@router.get("/")
+async def send_echo_message():
     """
     Sends echo back to user.
 
     :param incoming_message: incoming message.
     :returns: message same as the incoming.
     """
-    return incoming_message
+    return {"message": 'Hello World'}
